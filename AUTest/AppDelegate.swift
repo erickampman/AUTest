ccc1655f1c9df430299cf2666583d1e1e9d794f6
Eric Kampman <erickampman@yahoo.com>
2016-11-18 17:36:55 -0800

    Initial Commit

---
+++ AUTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AUTest
+//
+//  Created by Eric Kampman on 11/18/16.
+//  Copyright © 2016 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
