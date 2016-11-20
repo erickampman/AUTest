501276c09bc4ba6e9dec670ed6655b9049538486
Eric Kampman <erickampman@yahoo.com>
2016-11-20 12:44:29 -0800

    AU Views work. There was a problem with switching between synth and effect UIs. The popover view would not be correct for the effect (which is much bigger than the synth view) after the synth view was displayed. I fixed this by adding two popovers to the XIB file, which is ugly, and not generalizable. I'm not going to worry about this now, however.

--- AUTest/AUTestWindowController.swift
+++ AUTest/AUTestWindowController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import AVFoundation
+import CoreAudioKit
 
 class AUTestWindowController: NSWindowController {
 
@@ -17,6 +19,11 @@ class AUTestWindowController: NSWindowController {
 		auController.setupEffect()
     }
     
+	dynamic var playButtonTitle: String {
+		get {
+			return auController.isPlaying ? "Stop" : "Play"
+		}
+	}
 	@IBAction func playStop(_ sender: NSButton) {
 		if auController.isPlaying {
 			auController.stopPlaying()
@@ -25,16 +32,47 @@ class AUTestWindowController: NSWindowController {
 		}
 	}
 	
-	dynamic var playButtonTitle: String {
-		get {
-			return auController.isPlaying ? "Stop" : "Play"
+	@IBAction func showSynth(_ sender: NSButton) {
+		guard let au = auController.synthAU?.auAudioUnit else {
+			return
+		}
+
+		au.requestViewController { [weak self] viewController in
+			guard let strongSelf = self else { return }
+			
+			guard let viewController = viewController else { return }
+			
+			//			let view = viewController.view
+			
+			strongSelf.synthPopover.contentViewController = viewController
+			
+			strongSelf.synthPopover.show(relativeTo: sender.bounds, of: sender,
+			                        preferredEdge: NSRectEdge.maxX)
+		}
+	}
+	
+	@IBAction func showEffect(_ sender: NSButton) {
+		guard let au = auController.effectAU?.auAudioUnit else {
+			return
+		}
+		
+		au.requestViewController { [weak self] viewController in
+			guard let strongSelf = self else { return }
+			
+			guard let viewController = viewController else { return }
+			
+			strongSelf.effectPopover.contentViewController = viewController
+			
+			strongSelf.effectPopover.show(relativeTo: sender.bounds, of: sender,
+			                        preferredEdge: NSRectEdge.maxX)
 		}
 	}
 	
 	@IBOutlet weak var showSynthButton: NSButton!
 	@IBOutlet weak var showEffectButton: NSButton!
 	@IBOutlet weak var playButton: NSButton!
-	@IBOutlet var popover: NSPopover!
+	@IBOutlet var synthPopover: NSPopover!
+	@IBOutlet var effectPopover: NSPopover!
 	
 	dynamic let auController = AUController()
 }
